479df4cf80f566100ff8d0d6f20672c1e9d6c155
CoderDream <coderdream@gmail.com>
2022-05-28 10:01:05 +0800

    更新

--- 力扣/图解算法数据结构/graphic-data-structure/src/main/java/com/coderdream/ch10_math/Offer_66.java
+++ 力扣/图解算法数据结构/graphic-data-structure/src/main/java/com/coderdream/ch10_math/Offer_66.java
@@ -17,6 +17,8 @@ package com.coderdream.ch10_math;
  * 链接：https://leetcode.cn/leetbook/read/illustration-of-algorithm/57d8cm/
  * 来源：力扣（LeetCode）
  * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ * <p>
+ * https://www.bilibili.com/video/BV18D4y1D7JP
  */
 public class Offer_66 {
 
@@ -49,9 +51,12 @@ public class Offer_66 {
         return b;
     }
 
-    public int[] constructArrB(int[] nums) {
+    public int[] constructArrB(int[] a) {
         // 数组长度
-        int N = nums.length;
+        int N = a.length;
+        if (N == 0) {
+            return new int[0];
+        }
         // 输出数组
         int[] output = new int[N];
         // 输出数组的第一个元素置为1；
@@ -59,16 +64,16 @@ public class Offer_66 {
         // 设置左边数组为输出数组，从第二个元素开始相乘
         for (int i = 1; i < N; i++) {
             // 所在元素等于前一个元素乘原始数组的前一个元素
-            output[i] = output[i - 1] * nums[i - 1];
+            output[i] = output[i - 1] * a[i - 1];
         }
         // 右边数组的最后一个元素置为1；
         int rightOutput = 1;
         // 设置右边数组，从倒数第二个元素开始相乘
         for (int i = N - 1; i >= 0; i--) {
             // 设置输出数组对应位置的值，为左边乘积对应位置的值*右边上次计算后的值
-            output[i] =  output[i] * rightOutput;
+            output[i] = output[i] * rightOutput;
             // 所在元素等于后一个元素乘原始数组的当前元素
-            rightOutput = rightOutput * nums[i];
+            rightOutput = rightOutput * a[i];
         }
 
         return output;
